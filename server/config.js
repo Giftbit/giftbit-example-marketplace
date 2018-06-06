083a9a635dd1f984be4927c9ea4a13bf9c397f80
jeffmellis <jeffm@giftbit.com>
2018-06-06 12:14:53 -0700

    removed debugging code

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJTSEEyNTYifQ==.c05jek03anVDQTZXUE9NSjhCajBTRUJPeEFKeUwxS1pYbWFtaFIzdEZrclhlbG5hanNETUlNcitSbmFBQkg3aHRXWlpZZExCQ2I2dUhDd3pCQzlvQjVSK2xHcnkwcGwxbzFnMTBvbHNDRXI0ajBIbi9vTU1nY2tsTDVURnVPclQ=.9j19oryGIzsDMJaCjEzt4ukIs1tDENRQjaZMsaquFTE=',
-    giftTemplate: 'OTEUR'
+    token: '<Your Api Token Here>',
+    giftTemplate: '<Your Gift Template Id Here>'
 };
 
 module.exports = config;
